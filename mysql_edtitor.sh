de11a6d16f2e68bc7845e944cd347df46805b80d
wasdo129 <109848340+wasdo129@users.noreply.github.com>
2022-10-29 21:08:02 +0300

    Update mysql_edtitor.sh

--- mysql_edtitor.sh
+++ mysql_edtitor.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+db_name = 
+pass = 
 for i in $(cat text.txt)
 do
-echo "use cu44668_lbyis;" | mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
-#echo "CREATE TABLE $i (name VARCHAR(40), street VARCHAR(40));" |mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
-echo "DROP TABLE $i;" |mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
+echo "use db_name;" | mysql -u'db_name' -p'pass' db_name
+#echo "CREATE TABLE $i (name VARCHAR(40), street VARCHAR(40));" |mysql -u'db_name' -p'pass' db_name
+echo "DROP TABLE $i;" |mysql -u'db_name' -p'pass' db_name
 done
-echo "show tables;" | mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
+echo "show tables;" | mysql -u'db_name' -p'pass' db_name
