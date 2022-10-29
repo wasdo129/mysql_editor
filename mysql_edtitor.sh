033a92d472d10ae425ae76f639b382650d7ee4c9
wasdo129 <109848340+wasdo129@users.noreply.github.com>
2022-10-29 20:54:00 +0300

    Update mysql_edtitor.sh

--- mysql_edtitor.sh
+++ mysql_edtitor.sh
@@ -2,6 +2,6 @@
 
 echo "use cu44668_lbyis;" | mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
 #echo "CREATE TABLE $1 (name VARCHAR(40), street VARCHAR(40));" |mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
-echo "DELETE TABLE $1 (name VARCHAR(40), street VARCHAR(40));" |mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
+echo "DROP TABLE $1;" |mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
 
 echo "show tables;" | mysql -u'cu44668_lbyis' -p'5t2aTZwH' cu44668_lbyis
