b5f55f004da53acfe42bbeaf796fc811896fc84e
wasdo129 <109848340+wasdo129@users.noreply.github.com>
2022-10-29 21:08:33 +0300

    Update mysql_edtitor.sh

--- mysql_edtitor.sh
+++ mysql_edtitor.sh
@@ -5,7 +5,7 @@ pass =
 for i in $(cat text.txt)
 do
 echo "use db_name;" | mysql -u'db_name' -p'pass' db_name
-#echo "CREATE TABLE $i (name VARCHAR(40), street VARCHAR(40));" |mysql -u'db_name' -p'pass' db_name
-echo "DROP TABLE $i;" |mysql -u'db_name' -p'pass' db_name
+echo "CREATE TABLE $i (name VARCHAR(40), street VARCHAR(40));" |mysql -u'db_name' -p'pass' db_name
+#echo "DROP TABLE $i;" |mysql -u'db_name' -p'pass' db_name
 done
 echo "show tables;" | mysql -u'db_name' -p'pass' db_name
